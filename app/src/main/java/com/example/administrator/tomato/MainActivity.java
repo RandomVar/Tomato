6c5a51dac7bb9aa42f11ad13c16abc998c58c8b5
RandomVar <1035909127@qq.com>
2019-03-06 12:28:53 +0800

    修了PagerSlidingTabStrip

--- app/src/main/java/com/example/administrator/tomato/MainActivity.java
+++ app/src/main/java/com/example/administrator/tomato/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-import com.astuetz.PagerSlidingTabStrip;
 
 public class MainActivity extends AppCompatActivity {
 
